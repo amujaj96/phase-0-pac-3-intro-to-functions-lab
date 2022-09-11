c9fb759d55c7c57a23d1347763bcfbbf5c54c911
Agim <dawnhillsnyc@gmail.com>
2022-09-11 10:55:53 -0400

    commit tree b9a68c1da3a51b1991704873559d620e2fd57e7a

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+    return string;
+  }
+  "Hello!".toUpperCase(); // 'HELLO!'
+  function shout(string) {
+    return string.toUpperCase();
+  }
+
+  function whisper(string) {
+    return string;
+  }
+  "HELLO!".toLowerCase();
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+
+  function logShout(string) {
+    console.log(logShout)
+  }
+  "Hello!".toUpperCase
+  function logShout(string) {
+    console.log(logShout)
+  }
